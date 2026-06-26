e35f41144bab47c2f727769bbe66595cdda82411
dufontdd <dufont.dd@gmail.com>
2026-06-26 19:41:14 +0200

    feat: creat game window and implement backgound image

--- docs/fighter_games/main.py
+++ docs/fighter_games/main.py
@@ -6,27 +6,32 @@ pygame.init()
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
 
-screen = pygame.display.set_mode((SCREN_WIDTH? SCREEN_HEIGHT))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("brawler")
 
 #function for drawing background
 def draw_bg():
-    screen.blit(bg_images, (0, 0))
+    scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.blit(scaled_bg, (0, 0))
 
 #load background image
-bg_image = pygame.image.load("assets/images/background/background.jpg").convert_alpha()
+bg_image = pygame.image.load("asset/images/background/background.png").convert_alpha()
 
 #game loop
 run = True
 while run:
 
     #draw background
-    draw_bg
+    draw_bg()
+
     #event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+    
 
+    #update display
+    pygame.display.update() 
 
 #exit pygame
 pygame.quit()
